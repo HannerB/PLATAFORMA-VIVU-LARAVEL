9b22a374c0ec535f5d898a44e48314279116ca59
HannerB <hannerb48@gmail.com>
2024-10-02 17:16:01 -0500

    AGG - Agregada vista de login (BETA).

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,108 @@
 @extends('layouts.app')
 
+@section('title', 'Iniciar sesión | Oferta Complementaria')
+
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+    <div class="mt-1 PopUpContainer">
+        <div class="contentContainer">
+            <ol class="breadcrumb">
+                <li><a class="text-success" href="{{ route('home') }}">Inicio</a></li>
+                <li class="active">Iniciar sesión</li>
+            </ol>
+        </div>
+    </div>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
+    <div class="container down">
+        <div class="container">
+            @if (session('status'))
+                <div class="alert alert-success alert-dismissible" role="alert">
+                    {{ session('status') }}
+                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+            @endif
 
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
+            @if (session('error'))
+                <div class="alert alert-danger alert-dismissible" role="alert">
+                    {{ session('error') }}
+                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+            @endif
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
+            <div class="row">
+                <div class="col-xs-12 col-sm-6 col-sm-offset-3">
+                    <div class="full-width container-login">
+                        <i class="fa fa-user container-login-icon" aria-hidden="true"></i>
+                        <h2 class="text-center text-light">Iniciar Sesión</h2>
+                        <br>
+                        <form method="POST" action="{{ route('login') }}">
+                            @csrf
+                            <div class="form-group">
+                                <label for="documento"><i class="fa fa-user" aria-hidden="true"></i> Número de
+                                    documento</label>
+                                <input id="documento" type="number"
+                                    class="form-control @error('documento') is-invalid @enderror" name="documento"
+                                    value="{{ old('documento') }}" required autocomplete="documento" autofocus
+                                    placeholder="Usuario">
+                                @error('documento')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
                                 @enderror
                             </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
+                            <div class="form-group">
+                                <label for="password"><i class="fa fa-key" aria-hidden="true"></i> Contraseña</label>
+                                <input id="password" type="password"
+                                    class="form-control @error('password') is-invalid @enderror" name="password" required
+                                    autocomplete="current-password" placeholder="Contraseña">
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
                                 @enderror
                             </div>
+                            <a href="#" data-toggle="modal" data-target="#passwordResetModal"><i class="fa fa-key"
+                                    aria-hidden="true"></i> ¿Olvidaste tu contraseña?</a>
+                            <button class="btn btn-success btn-block" type="submit">Iniciar sesión</button>
+                        </form>
+                        <a href="{{ route('register') }}" class="btn btn-success btn-block">Registrarme</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Modal para recuperar contraseña -->
+    <div class="modal fade" id="passwordResetModal" tabindex="-1" role="dialog" aria-labelledby="passwordResetModalLabel"
+        aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="passwordResetModalLabel" style="font-size: 28px; text-align:center;">
+                        <i class="fa fa-envelope-open" aria-hidden="true"></i> ¿Olvidaste tu contraseña?
+                    </h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <form method="POST" action="{{ route('password.email') }}">
+                        @csrf
+                        <div class="form-group">
+                            <label for="documento"><i class="fa fa-envelope" aria-hidden="true"></i> Documento:</label>
+                            <input type="number" class="form-control" id="documento" name="documento" required>
                         </div>
-
-                        <div class="row mb-3">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
+                        <div class="modal-footer">
+                            <button type="submit" class="btn"
+                                style="background-color: #FF6C00; color: white; border-color: #FF6C00;">Recuperar
+                                contraseña</button>
                         </div>
                     </form>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,5 +1,22 @@
-<footer class="footerew">
+<style type="text/css">
+    .footer_new {
+        bottom: 0;
+        text-align: center;
+        font-size: 15px;
+        width: 100%;
+        height: 55px;
+        /* Set the fixed height of the footer here */
+        line-height: 44px;
+        /* Vertically center the text there */
+        background-color: #00AF00 !important;
+        color: white;
+    }
+</style>
+
+<footer class="footer_new">
     <div class="container">
-        <p>&copy; {{ date('Y') }} SENA. Todos los derechos reservados.</p>
+        <span class="">Todos los derechos <?php echo '&copy';
+        echo date('Y'); ?> SENA - Políticas de privacidad y condiciones uso
+            Portal Web SENA</span>
     </div>
 </footer>
