3dcba52129e3daa68f64e45938f05c9e10b0a19a
HannerB <hannerb48@gmail.com>
2024-11-03 14:33:04 -0500

    CONF - Mostrando notificación de eliminación de inscrito.

--- resources/views/poa/cursos-detalle/show_cursos.blade.php
+++ resources/views/poa/cursos-detalle/show_cursos.blade.php
@@ -3,20 +3,16 @@
 @section('content')
     <div class="container">
         @if (session('success'))
-            <div class="alert alert-success alert-dismissible fade show" role="alert">
-                {{ session('success') }}
-                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
+            <div class="alert alert-success alert-dismissible" role="alert">
+                <button type="button" class="close" data-dismiss="alert">&times;</button>
+                <span class="icon-checkmark"></span> {{ session('success') }}
             </div>
         @endif
 
         @if (session('error'))
-            <div class="alert alert-danger alert-dismissible fade show" role="alert">
-                {{ session('error') }}
-                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
+            <div class="alert alert-danger alert-dismissible" role="alert">
+                <button type="button" class="close" data-dismiss="alert">&times;</button>
+                <span class="icon-checkmark"></span> {{ session('error') }}
             </div>
         @endif
 
